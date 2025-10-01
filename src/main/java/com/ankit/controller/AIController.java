9cd3465733be54d622582e94cf3461626025c9cd
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-01 19:22:18 +0530

    Initial commit

---
+++ src/main/java/com/ankit/controller/AIController.java
@@ -0,0 +1,19 @@
+package com.ankit.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.ankit.service.LlamaAiService;
+
+@RestController
+public class AIController {
+
+    @Autowired
+    private LlamaAiService aiService;
+
+    @GetMapping("/api/v1/generate")
+    public String generate(@RequestParam(value = "promptMessage") String promptMessage) {
+        return aiService.generateResult(promptMessage);
+    }
+}
